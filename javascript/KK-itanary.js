7e520747e5bd0b56acca7b73ce6d6cea0531fdc7
prasanjit2005 <royprasanjit963@gmail.com>
2024-11-26 18:22:02 +0530

    itinary update

--- javascript/KK-itanary.js
+++ javascript/KK-itanary.js
@@ -9,3 +9,23 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 3000); // Change slide every 3 seconds
+
+function Download(){
+  
+    // Specify the URL of the PDF file
+    const pdfUrl = '../img/itanary/KEDARKANTHA\ TREK\ \(1\).pdf'; // Replace with your file URL
+
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'KEDARKANTHA\ TREK\ \(1\).pdf'; // Set the default file name
+    link.style.display = 'none';
+
+    // Add the link to the body and trigger the download
+    document.body.appendChild(link);
+    link.click();
+
+    // Remove the link element after download
+    document.body.removeChild(link);
+  
+}
